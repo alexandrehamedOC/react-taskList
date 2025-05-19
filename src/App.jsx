9a54c7dc4f5f8560694fb5305972c6c1693e00dc
alexandre hamed <alexandre.hamed@oclock.school>
2025-05-20 00:19:31 +0200

    add handleSelect existing tasks

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import SelectedProject from "./components/SelectedProject.jsx";
 function App() {
   // ici on met en place des states permetttant de gerer les projets à afficher
   const [projectState, setProjectsState] = useState({
@@ -49,7 +50,18 @@ function handleAddProject(projectData){
   )
 }
 
-let content;
+function handleSelectProject(id){
+ setProjectsState(prevState => {
+  return{
+    ...prevState,
+    selectedProjectId: id
+  };
+ })
+};
+
+const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId);
+
+let content = <SelectedProject project={selectedProject} />;
 if(projectState.selectedProjectId === null) {
   content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
 } else if(projectState.selectedProjectId === undefined) {
@@ -60,7 +72,8 @@ if(projectState.selectedProjectId === null) {
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar 
         onStartAddProject={handleStartAddProject}
-        projects={projectState.projects}/>
+        projects={projectState.projects}
+        onSelectProject={handleSelectProject}/>
       {content}
     </main>
   );
